6e069d0713e4c6e32cc82e78e85eca188b548047
darkos11 <gouga.11@live.com>
2023-09-19 13:03:57 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const date = require ('./midelware/Date')
+const app = express()
+
+const port = 5001
+
+//route 
+app.get('/',(req,res)=>{
+    res.send('you did first express project ')
+})
+
+// jiben les fichiers html 
+app.use('/static',express.static(__dirname +'/public'))
+
+
+app.get('/homePage',date,(req,res)=>{
+    res.sendFile(__dirname + '/public/home.html')
+})
+
+app.get('/productPage',(req,res)=>{
+    res.sendFile(__dirname + '/public/produit.html')
+})
+
+app.get('/contact-us',(req,res)=>{
+    res.sendFile(__dirname + '/public/contact.html')
+})
+
+
+app.listen(port,err=>{
+err? console.log(err):console.log(`you are connected to the ${port}`)
+})
