72ebb37a9c7a4111867f7c96b60e0a7b010528d2
darkos11 <135987654+darkos11@users.noreply.github.com>
2023-09-19 13:06:25 +0100

    Update Date.js

--- midelware/Date.js
+++ midelware/Date.js
@@ -1,12 +1,13 @@
 
 const date=(req,res,next)=>{
-let date = new Date();
-let h =date.getHours()
-
-if( !(h >= 9 &&  h <= 17)    ){
-res.send('you are not welcome ')
-}
-next()
-}
-
-module.exports=date
+    let date = new Date();
+    let h =date.getHours();
+    let dayOfWeek = date.getDay();
+    
+    if(! (dayOfWeek >= 1 && dayOfWeek <= 5) && (h >= 9 &&  h <= 17)){
+    res.send('le site est accessible de 9 à 17 / de lundi a Vendredi  ')
+    }
+    next()
+    }
+    
+    module.exports=date
